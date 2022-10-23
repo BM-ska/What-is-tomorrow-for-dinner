0553fa76da840313ffdbeff358dd7c3c3acccdc4
BM-ska <barbaramoczulska1a@gmail.com>
2022-10-23 21:53:22 +0200

    first commit

---
+++ src/main/java/com/dinner/Whatistomorrowfordinner/WhatIsTomorrowForDinnerApplication.java
@@ -0,0 +1,13 @@
+package com.dinner.Whatistomorrowfordinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsTomorrowForDinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatIsTomorrowForDinnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinner/Whatistomorrowfordinner/WhatIsTomorrowForDinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinner.Whatistomorrowfordinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsTomorrowForDinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
