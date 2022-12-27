38f74f865cf8cb1669814275da771fb5d1961752
Basia <barbaramoczulska1a@gmail.com>
2022-12-27 15:17:35 +0100

    collapese

--- frontend/src/pages/RecipeBookPage.tsx
+++ frontend/src/pages/RecipeBookPage.tsx
@@ -2,11 +2,9 @@ import Box from "@mui/material/Box";
 import * as React from "react";
 
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import {Button, Col, Collapse, Form, Input, InputNumber, Row, Select, Space} from 'antd';
-import {DeleteOutlined, EditOutlined, PlusCircleOutlined} from '@ant-design/icons';
+import {Button, Col, Collapse, Row} from 'antd';
+import {DeleteOutlined, EditOutlined} from '@ant-design/icons';
 import {ListItem, ListItemText} from "@mui/material";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import RecipeBookHeader from "../components/RecipeBookHeader";
 
 const recipeTmp = ['Barszcz', 'Bigos', 'Pierogi']
@@ -16,17 +14,22 @@ const initialRecipeListTMP = [
     {
         id: 'a',
         name: 'Barszcz',
+        calories : 1234
     },
     {
         id: 'b',
         name: 'Bigos',
+        calories : 1
     },
     {
         id: 'c',
         name: 'Pierogi',
+        calories : 12
     }
 
 ];
+
+
 
 // const amountRecipesInBarszczTmp = new Map<string, Object>([
 //     ["burak", [500,"g"]],
@@ -40,11 +43,7 @@ const amountRecipesInBarszczTmp = new Map<string, number>([
     ["ziemniak", 2354]
 ]);
 
-function ConfirmDelete(s: string) {
-    if (window.confirm("Are you sure you want to delete " + s + " from your recipe book?")) {
 
-    }
-}
 
 
 const {Panel} = Collapse;
@@ -54,6 +53,11 @@ const onChange = (key: string | string[]) => {
 };
 
 function RecipeBookPage() {
+    function ConfirmDelete(item : {id : string, name : string, calories : number}) {
+        if (window.confirm("Are you sure you want to delete " + item.name + " from your recipe book?")) {
+            deleteRecipe(item.id)
+        }
+    }
 
     const [list, setList] = React.useState(initialRecipeListTMP);
 
@@ -63,35 +67,68 @@ function RecipeBookPage() {
     }
 
     function deleteRecipe(id: string) {
-
+        console.log("aaaaaaaaaaaaaaaaaaaaaa")
     }
+    // @ts-ignore
+    const List = ({list, onRemove}) => (
 
-    // const List = ({list, onRemove}) => (
-    //
-    //     <Box>
-    //     </Box>
-    // );
-    //
-    // const Item = ({item, onRemove}) => (
-    //     <Box>
-    //     </Box>
-    // );
+        <Box boxShadow={20}>
+            <Collapse onChange={onChange}>
+                {list.map((item:{id : string, name : string, calories : number}) => (
+                    <Item key={item.id} item={item} onRemove={onRemove}/>
+                ))}
+            </Collapse>
+        </Box>
+    );
 
 
-    const genExtra = (mealName: string) => (
+    // @ts-ignore
+    const Item = ({item, onRemove}) => (
+        // @ts-ignore
+        <Panel header={calories(item.name, item.calories)}
+               key={item.id}
+               extra={genExtra(item.name)}>
+            {ingredientTmp.map((value) => (
+                <ListItem
+                    key={value}
+                    disableGutters>
+
+
+                    <Col span={1} xs={{order: 1}} sm={{order: 1}} md={{order: 1}} lg={{order: 1}}>
+                        <ArrowRightIcon/>
+                    </Col>
+                    <Col span={1} xs={{order: 2}} sm={{order: 2}} md={{order: 2}} lg={{order: 2}}>
+                        {amountRecipesInBarszczTmp.get(`${value}`)}
+
+                    </Col>
+                    <Col span={1} xs={{order: 3}} sm={{order: 3}} md={{order: 3}} lg={{order: 3}}>
+                        g
+                    </Col>
+                    <Col span={21} xs={{order: 4}} sm={{order: 4}} md={{order: 4}} lg={{order: 4}}>
+                        <ListItemText primary={`${value}`}/>
+                    </Col>
+
+
+                </ListItem>
+            ))}
+        </Panel>
+    );
+
+
+    const genExtra = (item :{id : string, name : string, calories : number}) => (
         <Box>
-            <Button shape="circle" icon={<EditOutlined/>} href={`/${mealName}`}/>
+            <Button shape="circle" icon={<EditOutlined/>} href={`/${item.name}`}/>
             <Button shape="circle" icon={<DeleteOutlined/>} onClick={() => {
-                ConfirmDelete(mealName)
+                ConfirmDelete(item)
             }}/>
         </Box>
 
     );
 
-    const calories = (mealName: string, kcal: number) => (
+    const calories = (name : string, kcal : number) => (
         <Row>
             <Col span={11} xs={{order: 1}} sm={{order: 1}} md={{order: 1}} lg={{order: 1}}>
-                {mealName}
+                {name}
             </Col>
             <Col span={13} xs={{order: 2}} sm={{order: 2}} md={{order: 2}} lg={{order: 2}}>
                 {kcal} kcal/100g
@@ -106,39 +143,8 @@ function RecipeBookPage() {
 
             <RecipeBookHeader/>
 
-            <Box boxShadow={20}>
-                <Collapse onChange={onChange}>
-                    {recipeTmp.map((value) => (
-                        <Panel header={calories(`${value}`, 1234)}
-                               key={value}
-                               extra={genExtra(`${value}`)}>
-                            {ingredientTmp.map((value) => (
-                                <ListItem
-                                    key={value}
-                                    disableGutters>
+            <List list={list} onRemove={deleteRecipe}/>
 
-
-                                    <Col span={1} xs={{order: 1}} sm={{order: 1}} md={{order: 1}} lg={{order: 1}}>
-                                        <ArrowRightIcon/>
-                                    </Col>
-                                    <Col span={1} xs={{order: 2}} sm={{order: 2}} md={{order: 2}} lg={{order: 2}}>
-                                        {amountRecipesInBarszczTmp.get(`${value}`)}
-
-                                    </Col>
-                                    <Col span={1} xs={{order: 3}} sm={{order: 3}} md={{order: 3}} lg={{order: 3}}>
-                                        g
-                                    </Col>
-                                    <Col span={21} xs={{order: 4}} sm={{order: 4}} md={{order: 4}} lg={{order: 4}}>
-                                        <ListItemText primary={`${value}`}/>
-                                    </Col>
-
-
-                                </ListItem>
-                            ))}
-                        </Panel>
-                    ))}
-                </Collapse>
-            </Box>
         </div>
     );
 }
